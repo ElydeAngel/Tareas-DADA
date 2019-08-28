bba2090758325259edd1686ef6dd36c7338d2652
Єıчdє άиgєl <elydeangel@hotmail.com>
2019-08-27 23:47:49 -0500

    Funciones

---
+++ Funciones.py
@@ -0,0 +1,27 @@
+def suma():
+    return
+print("¿Que numeros deseas sumar?");
+x = int(input ("Digita primer numero:"))
+y = int(input ("Digita segundo digito:"))
+print(f"Esto es igual a: ", x+y)
+
+def resta():
+    return
+print("¿Que numeros deseas restar?");
+x = int(input ("Digita primer numero:"))
+y = int(input ("Digita segundo digito:"))
+print(f"Esto es igual a: ", x-y)
+
+def multiplicacion():
+    return
+print("¿Que numeros deseas multiplicar?");
+x = int(input ("Digita primer numero:"))
+y = int(input ("Digita segundo digito:"))
+print(f"Esto es igual a: ", x*y)
+
+def division():
+    return
+print("¿Que numeros deseas dividir?");
+x = float(input ("Digita primer numero:"))
+y = float(input ("Digita segundo digito:"))
+print(f"Esto es igual a: ", x/y)
